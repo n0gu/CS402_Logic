77564fbaa38f8139a633300f98b247281076dcb3
Sungyoon Jeong <syjjm2@gmail.com>
2017-04-07 22:32:51 +0900

    Finish coursework 2 basics

---
+++ coursework2/cnf/cnf.py
@@ -0,0 +1,174 @@
+'''
+CS402 Coursework 2 - CNF
+20140515 Sungyoon Jeong
+'''
+
+import sys, os, subprocess
+op = ['&', '|', '-', '>', '<', '=']
+
+'''
+Take polish notation boolean formula as input, and make a tuple with it.
+Tuple consists of (op, lit, lit) or (op, lit) where type(lit) is tuple or string.
+'''
+def perror():
+	print "Error while parsing... exiting!"
+	exit()
+
+def parse_formula(formula):
+	res, rest = _parse(formula.split(' '))
+	if rest:
+		perror()
+	return res
+
+def _parse(formula_split):
+	if len(formula_split) == 0:
+		perror()
+	if formula_split[0] == '-':
+		lit, rest = _parse(formula_split[1:])
+		return ((formula_split[0], lit), rest)
+	elif formula_split[0] in op:
+		lit1, rest1 = _parse(formula_split[1:])
+		lit2, rest2 = _parse(rest1)
+		return ((formula_split[0], lit1, lit2), rest2)
+	else:
+		return (formula_split[0], formula_split[1:])
+
+'''
+All these functions take tuples as input parameter.
+'''
+def eqfree(formula):
+	if formula[0] not in op:
+		return formula
+	elif formula[0] == '=':
+		return ('&', ('>', eqfree(formula[1]), eqfree(formula[2])), ('<', eqfree(formula[1]), eqfree(formula[2])))
+	elif formula[0] == '-':
+		return ('-', eqfree(formula[1]))
+	elif formula[0] in op:
+		return (formula[0], eqfree(formula[1]), eqfree(formula[2]))
+	else:
+		print "Should not arrive here"
+
+def revimplfree(formula):
+	if formula[0] not in op:
+		return formula
+	elif formula[0] == '<':
+		return ('>', revimplfree(formula[2]), revimplfree(formula[1]))
+	elif formula[0] == '-':
+		return ('-', revimplfree(formula[1]))
+	elif formula[0] in op:
+		return (formula[0], revimplfree(formula[1]), revimplfree(formula[2]))
+	else:
+		print "Should not arrive here"
+
+def implfree(formula):
+	if formula[0] not in op:
+		return formula
+	elif formula[0] == '>':
+		return ('|', implfree(('-', formula[1])), implfree(formula[2]))
+	elif formula[0] == '-':
+		return ('-', implfree(formula[1]))
+	elif formula[0] in op:
+		return (formula[0], implfree(formula[1]), implfree(formula[2]))
+	else:
+		print "Should not arrive here"
+
+def nnf(formula):
+	if formula[0] not in op:
+		return formula
+	elif formula[0] == '-':
+		if formula[1][0] == '-':
+			return nnf(formula[1][1])
+		elif formula[1][0] == '&':
+			return nnf(('|', ('-', formula[1][1]), ('-', formula[1][2])))
+		elif formula[1][0] == '|':
+			return nnf(('&', ('-', formula[1][1]), ('-', formula[1][2])))
+		else:
+			return formula
+	elif formula[0] == '&' or formula[0] == '|':
+		return (formula[0], nnf(formula[1]), nnf(formula[2]))
+	else:
+		print "Should not arrive here"
+
+def cnf(formula):
+	if formula[0] not in op or formula[0] == '-':
+		return formula
+	elif formula[0] == '&':
+		return ('&', cnf(formula[1]), cnf(formula[2]))
+	elif formula[0] == '|':
+		return distr(cnf(formula[1]), cnf(formula[2]))
+	else:
+		print "Should not arrive here"
+
+def distr(f1, f2):
+	if f1[0] == '&':
+		return ('&', distr(f1[1], f2), distr(f1[2], f2))
+	elif f2[0] == '&':
+		return ('&', distr(f1, f2[1]), distr(f1, f2[2]))
+	else:
+		return ('|', f1, f2)
+
+# Wrapper function
+def to_cnf(formula):
+	return cnf(nnf(implfree(revimplfree(eqfree(formula)))))
+
+'''
+Print tuple into various output formats
+'''
+def cnf_to_polish(formula):
+	magic = str(formula)
+	return magic.replace('(', '').replace(')', '').replace('\'', '').replace(',', '')
+
+def cnf_to_infix(formula):
+	if formula[0] == '&':
+		left = '(%s)' % cnf_to_infix(formula[1]) if formula[1][0] == '|' else cnf_to_infix(formula[1])
+		right = '(%s)' % cnf_to_infix(formula[2]) if formula[2][0] == '|' else cnf_to_infix(formula[2])
+		return '%s & %s' % (left, right)
+	elif formula[0] == '-':
+		return '- %s' % formula[1]
+	elif formula[0] == '|':
+		return '%s | %s' % (cnf_to_infix(formula[1]), cnf_to_infix(formula[2]))
+	else:
+		return formula
+
+def cnf_to_minisat(formula, in_filename):
+	lits = set()
+	magic = cnf_to_polish(formula).split(' ')
+	for c in magic:
+		if c not in op:
+			lits.add(c)
+	lits = list(lits)
+	lits.sort()
+
+	with open(in_filename, 'w') as f:
+		f.write('p cnf %d %d\n' % (len(lits), magic.count('&') + 1))
+		f.write(_to_minisat(formula, lits))
+		f.write(' 0')
+
+def _to_minisat(formula, lits):
+	if formula[0] == '&':
+		return _to_minisat(formula[1], lits) + ' 0\n' + _to_minisat(formula[2], lits)
+	elif formula[0] == '|':
+		return _to_minisat(formula[1], lits) + ' ' +  _to_minisat(formula[2], lits)
+	elif formula[0] == '-':
+		return '-' + _to_minisat(formula[1], lits)
+	else:
+		return str(lits.index(formula) + 1)
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print "Usage: python cnf.py inputfile"
+		exit()
+	f = open(sys.argv[1], 'r')
+	formula = parse_formula(f.readline().strip())
+	formula = to_cnf(formula)
+	print cnf_to_polish(formula)
+	print cnf_to_infix(formula)
+
+	minisat_input = "test.in"
+	minisat_output = "test.out"
+	cnf_to_minisat(formula, minisat_input)
+	result =  os.system("minisat %s %s > /dev/null" % (minisat_input, minisat_output))
+	with open(minisat_output, 'r') as out:
+		sat = out.readline().strip()
+	print sat
+	f.close()
---
+++ coursework2/nonogram/nonogram.py
@@ -0,0 +1,203 @@
+import os, sys
+from cnf import to_cnf, _to_minisat
+
+'''______________
+  /             /|
+ /             / |
+/_____________/  |
+|             |  /
+|   O  O  O   | /
+|_____________|/
+
+This is a box.
+The nonogram solver you asked for is inside.
+'''
+
+# Check validity of input file.
+def check_validity(input_fname):
+	try:
+		f = open(input_fname, 'r')
+		row = int(f.readline().strip())
+		col = int(f.readline().strip())
+		for i in xrange(row):
+			if minlen(map(int, f.readline().strip().split(' '))) > col:
+				f.close()
+				return False
+		for i in xrange(col):
+			if minlen(map(int, f.readline().strip().split(' '))) > row:
+				f.close()
+				return False
+		f.close()
+		return True
+	except IOError:
+		print "Error: File does not exist"
+		return False
+	except ValueError:
+		print "Error: Invalid input in input file"
+		return False
+
+# Calculate minimal length of clues.
+def minlen(l):
+	s = 0
+	for i in l:
+		if i < 0:
+			raise ValueError
+		if i == 0:
+			if len(l) == 1:
+				return 0
+			else:
+				raise ValueError
+		s += (i + 1)
+	s -= 1
+	return s
+
+# Wrapper function, just to make cumulating easier.
+def add(orig_clause, new_clause, op):
+	if orig_clause:
+		return (op, new_clause, orig_clause)
+	else:
+		return new_clause
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print "Usage: python cnf.py inputfile"
+		exit()
+	if not check_validity(sys.argv[1]):
+		exit()
+
+	f_in = open(sys.argv[1], 'r')
+
+	row = int(f_in.readline().strip())
+	col = int(f_in.readline().strip())
+
+	row_clues = []
+	col_clues = []
+	literal = []
+	nonogram_formula = None
+
+	for r in xrange(row):
+		row_clues.append(map(int, f_in.readline().strip().split(' ')))
+	for c in xrange(col):
+		col_clues.append(map(int, f_in.readline().strip().split(' ')))
+
+	print "Step 1: Making nonogram into logical formula"
+	for r in range(row):
+		row_clue = row_clues[r]
+		if minlen(row_clue) == 0:
+			and_st = None
+			for j in xrange(col):
+				literal.append('R%d_%d_%d' % (r, 0, j))
+				and_st = add(and_st, ('-', 'R%d_%d_%d' % (r, 0, j)), '&')
+				and_st = add(and_st, ('-', 'X%d_%d' % (r, j)), '&')
+			nonogram_formula = add(nonogram_formula, and_st, '&')
+		else:
+			chunk_cumulative = 0
+			entropy = col - minlen(row_clue) + 1
+			for i in xrange(len(row_clue)):
+				and_st = None
+				or_st = None
+				for j in xrange(col):
+					row_chunk_literal = 'R%d_%d_%d' % (r, i, j)
+					literal.append(row_chunk_literal)
+					if (j >= chunk_cumulative) and (j < chunk_cumulative + entropy):
+						or_st = add(or_st, row_chunk_literal, '|')
+					else:
+						and_st = add(and_st, ('-', row_chunk_literal), '&')
+				and_st = add(and_st, or_st, '&')
+
+				for j in xrange(chunk_cumulative, chunk_cumulative + entropy):
+					temp_st = None
+					for k in xrange(col):
+						if k != j:
+							temp_st = add(temp_st, ('-', 'R%d_%d_%d' % (r, i, k)), '&')
+					if i != len(row_clue) - 1:
+						for k in xrange(0, j + row_clue[i] + 1):
+							temp_st = add(temp_st, ('-', 'R%d_%d_%d' % (r, i+1, k)), '&')
+					for k in xrange(j, j + row_clue[i]):
+						temp_st = add(temp_st, 'X%d_%d' % (r, k), '&')
+					temp_st = ('>', 'R%d_%d_%d' % (r, i, j), temp_st)
+					and_st = add(temp_st, and_st, '&')
+				chunk_cumulative += row_clue[i] + 1
+				nonogram_formula = add(nonogram_formula, and_st, '&')
+
+	for c in range(col):
+		col_clue = col_clues[c]
+		if minlen(col_clue) == 0:
+			and_st = None
+			for j in xrange(row):
+				literal.append('C%d_%d_%d' % (c, 0, j))
+				and_st = add(and_st, ('-', 'C%d_%d_%d' % (c, 0, j)), '&')
+				and_st = add(and_st, ('-', 'X%d_%d' % (j, c)), '&')
+			nonogram_formula = add(nonogram_formula, and_st, '&')
+		else:
+			chunk_cumulative = 0
+			entropy = row - minlen(col_clue) + 1
+			for i in xrange(len(col_clue)):
+				and_st = None
+				or_st = None
+				for j in xrange(col):
+					col_chunk_literal = 'C%d_%d_%d' % (c, i, j)
+					literal.append(col_chunk_literal)
+					if (j >= chunk_cumulative) and (j < chunk_cumulative + entropy):
+						or_st = add(or_st, col_chunk_literal, '|')
+					else:
+						and_st = add(and_st, ('-', col_chunk_literal), '&')
+				and_st = add(and_st, or_st, '&')
+				
+				for j in xrange(chunk_cumulative, chunk_cumulative + entropy):
+					temp_st = None
+					for k in xrange(row):
+						if k != j:
+							temp_st = add(temp_st, ('-', 'C%d_%d_%d' % (c, i, k)), '&')
+					if i != len(col_clue) - 1:
+						for k in xrange(0, j + col_clue[i] + 1):
+							temp_st = add(temp_st, ('-', 'C%d_%d_%d' % (c, i+1, k)), '&')
+					for k in xrange(j, j + col_clue[i]):
+						temp_st = add(temp_st, 'X%d_%d' % (k, c), '&')
+					temp_st = ('>', 'C%d_%d_%d' % (c, i, j), temp_st)
+					and_st = add(temp_st, and_st, '&')
+				chunk_cumulative += col_clue[i] + 1
+				nonogram_formula = add(nonogram_formula, and_st, '&')
+
+	for r in range(row):
+		for c in range(col):
+			literal.append('X%d_%d' % (r, c))
+			row_st = None
+			col_st = None
+			for i in xrange(len(row_clues[r])):
+				start = max(0, c - row_clues[r][i] + 1)
+				end = min(c, col - row_clues[r][i])
+				for j in xrange(start, end + 1):
+					row_st = add(row_st, 'R%d_%d_%d' % (r, i, j), '|')
+			for i in xrange(len(col_clues[c])):
+				start = max(0, r - col_clues[c][i] + 1)
+				end = min(r, row - col_clues[c][i])
+				for j in xrange(start, end + 1):
+					col_st = add(col_st, 'C%d_%d_%d' % (c, i, j), '|')
+			nonogram_formula = add(nonogram_formula, ('>', 'X%d_%d' % (r, c), ('&', row_st, col_st)), '&')
+
+	print "Step 2: Converting to cnf form..."
+	nonogram_formula = to_cnf(nonogram_formula)
+
+	with open('minisat_in', 'w') as f:
+		f.write('p cnf %d %d\n' % (len(literal), str(nonogram_formula).count('&') + 1))
+		f.write(_to_minisat(nonogram_formula, literal))
+		f.write(' 0')
+		f.close()
+
+	print "Step 3: Using minisat to solve formula..."
+	os.system("minisat %s %s > /dev/null" % ('minisat_in', 'minisat_out'))
+	with open('minisat_out', 'r') as out:
+		sat = out.readline().strip()
+		res = out.readline().strip().split(' ')
+
+	for r in range(row):
+		row_str = ""
+		for c in range(col):
+			pix_idx = literal.index('X%d_%d' % (r, c)) + 1
+			if str(pix_idx) in res:
+				row_str += '#'
+			elif '-' + str(pix_idx) in res:
+				row_str += '.'
+		print row_str
